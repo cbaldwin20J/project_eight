dc69b83885a6fc96d8ffcf0031b1892c8b337b90
Chris Baldwin <cbaldwin20@gmail.com>
2018-08-08 23:51:14 -0500

    Get the javascript concat to work

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
 // 'use strict' is a javascript thing, not necessary but useful.
 'use strict';
 
-const gulp = require('gulp');
+const gulp = require('gulp'),
+	concat = require('gulp-concat');
 
-// so to run this task, in the console we do 'gulp hello'.
-gulp.task('hello', function(){
-	console.log("Hello!");
+// so to run this task, in the console we do 'gulp concatScripts'.
+gulp.task('concatScripts', function(){
+	// here we gather all of our javascript files.
+	// make sure to order them the way you would in an index.html file.
+	gulp.src([
+		'js/global.js',
+		'js/circle/autogrow.js',
+		'js/circle/circle.js'])
+	// this concats them all into this one file 'app.js'
+	// (it will create the file for us)
+	.pipe(concat('app.js'))
+	// this will put the 'app.js' file in the folder 'js'.
+	.pipe(gulp.dest("js"));
 });
 
 // to run this task, because its 'default' in the console
